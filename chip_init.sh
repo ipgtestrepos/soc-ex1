b8dc8fb446fac3f5bd0e03a95c712ed25a4bad2d
Dan FitzPatrick <dan.fitzpatrick@silvaco.com>
2024-12-12 11:25:57 -0500

    precautionary cleanup

--- chip_init.sh
+++ chip_init.sh
@@ -3,6 +3,9 @@ echo ===========================================================================
 echo BEGIN FILE: chip_init.sh
 echo -e "===========================================================================\n"
 
+# cleanup 
+rm -rf .ipg
+
 # initialize the chip for 'mips' using the latest version
 description="MIPS SoC Development Project"
 ipg chip init --chip mips --version 1.0.0 --internal-name internal-soc-mips --description "$description"
